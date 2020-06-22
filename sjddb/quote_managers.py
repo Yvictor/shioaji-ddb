3fac7993f95c1ecfe3e6351cded3edda181a5488
yp0chien <ypochien@gmail.com>
2020-06-23 04:37:50 +0800

    feat: complete TickTFE. 1. add TFE tick column 2. upload tick to ddb 3 add tfe dfs table 4. add subscribe tfe stream to dfs

--- sjddb/quote_managers.py
+++ sjddb/quote_managers.py
@@ -1,7 +1,7 @@
 import redis
 import msgpack
 import dolphindb
-
+import pandas as pd
 from sjddb.utils import flatten_tick
 
 
@@ -18,7 +18,18 @@ class DDBQuoteManager:
         self.ddb = ddb
 
     def on_quote(self, topic: str, quote: dict):
-        if topic.startswith("L") or topic.startswith("MKT"):
-            for index, _ in enumerate(quote.get("Code", [])):
+        if topic.startswith("L"):  # or topic.startswith("MKT"):
+            ticks = []
+            for index, _ in enumerate(quote.get("Close", [])):
                 tick = flatten_tick(quote, index)
-                # push tick
+                tick["SubSeq"] = tick["index"]
+                tick["Exchange"] = "TFE"
+                tick["Date"] = tick["Date"].replace("/", ".")
+                tick["Time"] = tick["Time"][0:-3]
+                ticks.append(tick)
+
+            df = pd.DataFrame.from_dict(ticks, orient="columns")
+            self.ddb.upload({"tStreamTickTFE": df})
+            self.ddb.run(
+                "objByName('StreamTickTFE').append!(select Exchange,index,Amount,AmountSum,AvgPrice,Close,Code,date(Date),DiffPrice,DiffRate,DiffType,High,Low,Open,TargetKindPrice,TickType,time(Time),TradeAskVolSum,TradeBidVolSum,VolSum,Volume from tStreamTickTFE)"
+            )
